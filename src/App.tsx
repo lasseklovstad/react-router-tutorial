2628ebe87d837ef20cd036cafb19e510b89987ca
Lasse Kløvstad <lasse.klovstad@ciber.no>
2023-06-05 21:56:35 +0200

    Example 1.5 convert to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/Header";
-import { Home } from "./routes/Home";
-import { Todos } from "./routes/Todos";
-import { NewTodo } from "./routes/NewTodo";
 import { EditTodo } from "./routes/EditTodo";
+import { NewTodo } from "./routes/NewTodo";
+import { Todos } from "./routes/Todos";
+import { Home } from "./routes/Home";
+
+const RootLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "home", element: <Home /> },
+      {
+        path: "todos",
+        children: [
+          { path: "new", element: <NewTodo /> },
+          { path: ":todoId", element: <EditTodo /> },
+          { index: true, element: <Todos /> },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="home" element={<Home />} />
-        <Route path="todos">
-          <Route path="new" element={<NewTodo />} />
-          <Route path=":todoId" element={<EditTodo />} />
-          <Route index element={<Todos />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
